76fc24513e7d2bd1e4a05276c81b9b790dd46a0f
Nyamburavee <veronyambura716@gmail.com>
2023-09-28 06:00:32 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -16,7 +16,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 	for (i = 63; i >= 0; i--)
 	{
-		present_val = exclusive >> i;
+		present_val = exclusive_val >> i;
 		if (present_val & 1)
 			count++;
 	}
